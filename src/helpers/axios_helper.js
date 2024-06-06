9aa2091b85fdc54f86c6154fa42c50b89bfc6989
ZaharSirko <magezku@gmail.com>
2024-06-06 23:53:33 +0300

    commit check token expire

--- src/helpers/axios_helper.js
+++ src/helpers/axios_helper.js
@@ -1,35 +1,57 @@
 import axios from 'axios';
 
+const TOKEN_EXPIRY_TIME = 60*60*1000; // Час у мілісекундах, наприклад 1 година (3600000 мс)
 
 export const getAuthToken = () => {
-    return window.localStorage.getItem('auth_token');
+    const token = window.localStorage.getItem('auth_token');
+    const tokenExpiry = window.localStorage.getItem('auth_token_expiry');
+
+    if (token && tokenExpiry) {
+        const now = new Date().getTime();
+        if (now >= tokenExpiry) {
+            clearAuthHeader();
+            return null;
+        }
+        return token;
+    }
+    return null;
 };
 
 export const setAuthHeader = (token) => {
     if (token !== null) {
+        const now = new Date().getTime();
+        const expiryTime = now + TOKEN_EXPIRY_TIME;
         window.localStorage.setItem("auth_token", token);
+        window.localStorage.setItem("auth_token_expiry", expiryTime);
     } else {
-        window.localStorage.removeItem("auth_token");
+        clearAuthHeader();
     }
 };
 
 export const clearAuthHeader = () => {
     window.localStorage.removeItem("auth_token");
+    window.localStorage.removeItem("auth_token_expiry");
 };
 
 axios.defaults.baseURL = 'http://localhost:8080';
 // axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const request = (method, url, data) => {
-
     let headers = {};
-    if (getAuthToken() !== null && getAuthToken() !== "null") {
-        headers = {'Authorization': `Bearer ${getAuthToken()}`};
+    const token = getAuthToken();
+    if (token) {
+        headers = {'Authorization': `Bearer ${token}`};
     }
 
     return axios({
         method: method,
         url: url,
         headers: headers,
-        data: data});
+        data: data
+    });
 };
+
+// Запускаємо перевірку на кожні 10 хвилин (600000 мс)
+setInterval(() => {
+    getAuthToken();
+}, 60*60*1000);
